bcf281b7515a41fc2374771e1f1f6aa015bbf5dc
rachelcherry <rcherry@bu.edu>
2024-02-28 21:56:56 -0500

    added in the ArticleList component to the news page so that we can see the list of articles

--- src/pages/news/index.tsx
+++ src/pages/news/index.tsx
@@ -7,32 +7,41 @@ import ArticleTable from "@/components/ArticleTable";
 import ArticleStatistics from "@/components/ArticleStatistics";
 
 const NewsPage: React.FC = () => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const [pageSize, setPageSize] = useState(10);
   const [allArticles, setAllArticles] = useState(0);
-
   const [articles, setArticles] = useState<any[]>([]);
   const [wait, setWait] = useState(true);
   useEffect(() => {
+    // declare the async data fetching function
     const fetchData = async () => {
       try {
-        const data = await fetch(`https://api.spaceflightnewsapi.net/v4/articles?offset=0&limit=10&_sort=published_at:desc`);
+        const offset = 0
+        const limit = 10;
+        const data = await fetch(`https://api.spaceflightnewsapi.net/v4/articles?offset=${offset}&limit=${limit}&_sort=published_at:desc`);
         const json = await data.json();
         setArticles(json.results);
         setAllArticles(json.count);
       } catch (error) {
         console.error('Error fetching articles:', error);
-      } 
+      } finally {
+        setWait(false);
+      }
     };
 
+    // call the function
     fetchData()
-  
-  }, []);
+      // make sure to catch any error
 
+      // When a piece of state needs to trigger a re-render (or useEffect re-fire)
+      // it should be included in this dependency array.
+  }, []);
   
 
   return (
     <div style={{ width: "100%" }}>
-     
-      
+  <Typography.Title level={2}>Articles</Typography.Title>
+      {<ArticleList articleList={articles} wait={wait} />}
     </div>
   );
 };
