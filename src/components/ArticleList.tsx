6c5f781a700f93a6f23da3defaae344da161940d
rachelcherry <rcherry@bu.edu>
2024-02-28 21:53:30 -0500

    added list of articles to the pageand put them into a table

--- src/components/ArticleList.tsx
+++ src/components/ArticleList.tsx
@@ -1,21 +1,33 @@
 import React from "react";
-import { Card, Row, Skeleton } from "antd";
+import { Card, Row, Col, Skeleton } from "antd";
 import { Article } from "@/types/types";
 import ArticleCard from "./ArticleCard";
 
 // You'll need to replace undefined with the correct type
-const ArticleList: React.FC<undefined> = () => (
-  /**
-   * This component renders a list of articles. It takes as input an array of articles and a boolean indicating whether the list is loading.
-   * You will need to write a props interface for this component.
-   *
-   * You should use your custom ArticleCard component to build this.
-   * No data manipulation is needed here.
-   * Don't forget to add a unique key prop to each ArticleCard.
-   * Don't forget to add a Skeleton component for when the list is loading. You might need conditional render logic for this
-   *
-   */
-  <div>Build me</div>
+interface ArticleListProps {
+  articleList: Article[];
+  wait: boolean;
+}
+
+const ArticleList: React.FC<ArticleListProps> = ({ articleList, wait }) => (
+  <div>
+    <Row gutter={[24, 24]}>
+    {wait ? (
+    //  <Col span={24}>
+      <Skeleton active />
+   // </Col>
+    ) : (
+      articleList.map((article) => (
+        <Col key={article.id} md={8} >
+        <ArticleCard
+          key={article.id}
+          article={article}
+        />
+         </Col>
+      ))
+    )}
+    </Row>
+  </div>
 );
 
 export default ArticleList;
