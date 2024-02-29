344166287f7ad7df11dbd84ca3e7ecc805d3f86e
rachelcherry <rcherry@bu.edu>
2024-02-28 21:43:49 -0500

    added ArticleCard component that creates the card for each article and gives the error photo from the import if there is no photo present

--- src/components/ArticleCard.tsx
+++ src/components/ArticleCard.tsx
@@ -1,34 +1,48 @@
-/* eslint-disable @next/next/no-img-element */
 import React from "react";
-import { Card, Row, Col, Typography, Divider } from "antd";
+import { Card, Divider } from "antd";
 import { Article } from "@/types/types";
-// This gets you access to the image for 404 fallback.
-// The href/source/url can be retrieved via image404.src
 import image404 from "../assets/404.png";
+import { useState } from "react";
+
 
 interface ArticleCardProps {
   article: Article;
 }
 
-const ArticleCard: React.FC<ArticleCardProps> = ({ article }) => (
-  /**
-   * This component renders a single article. It takes as input an article object.
-   * You should read the Antd documentation to figure out how to build this.
-   * You should not use the Next.js Image component, instead use a regular img tag.
-   * You will need to handle 404s for images, you can use the image404 variable above.
-   * Don't forget to format the date correctly
-   *
-   * You should display the following information:
-   * 1. Title
-   * 2. Image
-   * 3. Summary
-   * 4. Published At
-   *
-   * When you click on this component, it should take you to the article's url(same tab or different is fine).
-   *
-   * Don't forget that you need to add a unique key prop to each ArticleCard.
-   */
-  <div>build me</div>
-);
+
+const ArticleCard: React.FC<ArticleCardProps> = ({ article }) => {
+  const [isError, setError] = useState(false);
+  const handleError = () => {
+    setError(true);
+  };
+  const handleClick = () => {
+    window.open(article.url);
+  };
+
+  return (
+    <div>
+      <Card
+        title={
+          <div style={{ display: "flex", alignItems: "center" }}>
+            <h4 style={{ margin: 0, flex: 1, overflow: "hidden", textOverflow: "ellipsis", justifyContent: "space-between" }}>{article.title}</h4>
+            <p style={{ fontWeight: "normal" }}>{new Date(article.published_at).toLocaleDateString("en-US", { weekday: "long", year: "numeric", month: "long", day: "numeric" })}</p>
+          </div>
+        }
+        cover={
+        isError ? (
+          <img src={image404.src} alt={"error"} />
+        ) : (
+        <img src={article.image_url} onError={handleError} alt={article.title} />)}
+        onClick={handleClick}
+        key={article.id}
+        hoverable
+      >
+        <Divider />
+        <h3><u>{article.title}</u></h3>
+        <p>{article.summary}</p>
+      </Card>
+    </div>
+  );
+};
 
 export default ArticleCard;
