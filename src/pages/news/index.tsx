8f620aacebba06f59232ed840c3ad6a6b00f3e31
rachelcherry <rcherry@bu.edu>
2024-02-28 21:24:05 -0500

    added useEffect for api call to set the articles

--- src/pages/news/index.tsx
+++ src/pages/news/index.tsx
@@ -1,18 +1,38 @@
 /* eslint-disable @next/next/no-img-element */
 import React, { useEffect, useState } from "react";
-import { Divider, Typography } from "antd";
+import { Divider, Typography, Switch, Pagination, PaginationProps } from "antd";
+import ArticleCard from "@/components/ArticleCard";
+import ArticleList from "@/components/ArticleList";
+import ArticleTable from "@/components/ArticleTable";
+import ArticleStatistics from "@/components/ArticleStatistics";
 
 const NewsPage: React.FC = () => {
+  const [allArticles, setAllArticles] = useState(0);
+
+  const [articles, setArticles] = useState<any[]>([]);
+  const [wait, setWait] = useState(true);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await fetch(`https://api.spaceflightnewsapi.net/v4/articles?offset=0&limit=10&_sort=published_at:desc`);
+        const json = await data.json();
+        setArticles(json.results);
+        setAllArticles(json.count);
+      } catch (error) {
+        console.error('Error fetching articles:', error);
+      } 
+    };
+
+    fetchData()
+  
+  }, []);
+
+  
+
   return (
     <div style={{ width: "100%" }}>
-      {/* You can delete this div if you want */}
-      <div style={{ marginBottom: "10px" }}>{/* Add Switch inside here */}</div>
-      <Divider />
-      <Typography.Title level={2}>Articles</Typography.Title>
-      {/* Add conditional render logic for table vs grad/list */}
-      {/* Add pagination control using Antd(lookup the component). The same one should be used for both the table and grid views */}
-      {/* It should be centered on the page */}
-      {/* When you change the page, or the items per page, it should reset the scroll to the top of the page */}
+     
+      
     </div>
   );
 };
