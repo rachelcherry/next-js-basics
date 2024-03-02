15e1a3958c1d3064863e11752c209b1f75080555
rachelcherry <rcherry@bu.edu>
2024-03-02 14:48:50 -0500

    got rid of the background of the footer and changed the wording to look like the example

--- src/layout/footer.tsx
+++ src/layout/footer.tsx
@@ -5,7 +5,7 @@ const { Footer } = Layout;
 const CustomFooter = () => { /* creates a footer tag for the page */
 
   return (
-    <Footer /* creates footer that looks like example */ style={{ textAlign: "center", backgroundColor: "#333", color: "white"}}>
+    <Footer /* creates footer that looks like example */ style={{ textAlign: "center", color: "black"}}>
        Spark! &copy;2023 Created by Spark!  
     </Footer >
   );
