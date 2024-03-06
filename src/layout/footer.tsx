0925e590a074660fc2abf635142cbc0192370b2d
rachelcherry <rcherry@bu.edu>
2024-03-05 19:36:20 -0500

    changed 2023 to 2024

--- src/layout/footer.tsx
+++ src/layout/footer.tsx
@@ -6,7 +6,7 @@ const CustomFooter = () => { /* creates a footer tag for the page */
 
   return (
     <Footer /* creates footer that looks like example */ style={{ textAlign: "center", color: "black"}}>
-       Spark! &copy;2023 Created by Spark!  
+       Spark! &copy;2024 Created by Spark!  
     </Footer >
   );
 };
